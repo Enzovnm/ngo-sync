85147c03a40222f7053535fe2d993377d68481e7
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-10-31 16:30:28 -0300

    Creating API project

---
+++ api/NGOsync/src/test/java/com/monteiro/enzo/ngosync/NgOsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monteiro.enzo.ngosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgOsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
