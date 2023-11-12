0cd84c9c784e6f0e0b5a43a87cc3dc4b9c34b110
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-11-12 03:48:35 -0300

    Creating the method save on controller for Donor

--- api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/controllers/DonorController.java
+++ api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/controllers/DonorController.java
@@ -1,9 +1,28 @@
 package com.monteiro.enzo.ngosync.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.monteiro.enzo.ngosync.dtos.DonorDto;
+import com.monteiro.enzo.ngosync.services.DonorService;
 
 @RestController
 public class DonorController {
-
 	
+	private final DonorService donorService;
+	
+	@Autowired
+	public DonorController(DonorService donorService) {
+		this.donorService = donorService;
+	}
+
+	@PostMapping("ngos/{ngoId}/donors")
+	public ResponseEntity<DonorDto> save(@PathVariable(value = "ngoId") Long id, @RequestBody DonorDto donor){
+		return new ResponseEntity<DonorDto>(donorService.save(id, donor), HttpStatus.CREATED);
+	}
 }
--- api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/dtos/DonorDto.java
+++ api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/dtos/DonorDto.java
@@ -2,12 +2,6 @@ package com.monteiro.enzo.ngosync.dtos;
 
 import com.monteiro.enzo.ngosync.entities.Ngo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
