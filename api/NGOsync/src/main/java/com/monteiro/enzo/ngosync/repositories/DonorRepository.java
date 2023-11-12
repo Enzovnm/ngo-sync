f1e87b409ae289fafbd788c1c5186768db0f6a6e
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-11-12 03:47:06 -0300

    Adding Repository annotation

--- api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/repositories/DonorRepository.java
+++ api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/repositories/DonorRepository.java
@@ -1,9 +1,11 @@
 package com.monteiro.enzo.ngosync.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.monteiro.enzo.ngosync.entities.Donor;
 
+@Repository
 public interface DonorRepository extends JpaRepository<Donor, Long>{
 
 }
