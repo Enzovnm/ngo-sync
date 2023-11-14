39ae40a1f38ed50a0d87c50381b79dbd4c1d02a9
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-11-13 22:47:55 -0300

    Removing propertie ID in NgoDtoInsert

--- api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/dtos/NgoDtoInsert.java
+++ api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/dtos/NgoDtoInsert.java
@@ -1,6 +1,6 @@
 package com.monteiro.enzo.ngosync.dtos;
 
-public record NgoDtoInsert(long id, String name, String email,String password,String cnpj, String logo, String description, String site, String state, String city, String neighborhood,
+public record NgoDtoInsert(String name, String email,String password,String cnpj, String logo, String description, String site, String state, String city, String neighborhood,
 		String postalCode, String address, int addressNumber, String addressComplement, String phone) {
 
 }
