6f4909a6a65782cf5b31ee6397749913e262c7be
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-11-22 21:21:52 -0300

    Removing insert query

--- api/NGOsync/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO NGOS (name, email, password, cnpj, logo, description, site, state, city, neighborhood, POSTAL_CODE, address, ADDRESS_NUMBER, ADDRESS_COMPLEMENT, phone) VALUES ('Solidary NGO', 'solidary.suport@gmail.com', '123456789012345678901234567890123456789012345678901234567890', '46995466000175', 'logo', 'lorem ipsum', 'www.solidary.com', 'SP', 'São Paulo', 'Jardim Perdizes','01140060','Avenida Nicolas boer', 399, null, '11993567255');
-
-
-
-
-
-
