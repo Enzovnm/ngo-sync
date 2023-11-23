c635a3888ad7fb225cf4b6884782193642c53f64
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-11-23 12:56:10 -0300

    Creating methods to check if exists an 'email' and 'cpf'

--- api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/repositories/NgoRepository.java
+++ api/NGOsync/src/main/java/com/monteiro/enzo/ngosync/repositories/NgoRepository.java
@@ -6,5 +6,8 @@ import com.monteiro.enzo.ngosync.entities.Ngo;
 
 
 public interface NgoRepository extends JpaRepository<Ngo, Long>{
-	
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCnpj(String cnpj);
 }
